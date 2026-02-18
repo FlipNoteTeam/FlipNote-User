a28e7846360907d274b8db53f097cb80ffd108ce
dungbik <q1w2e30630@gmail.com>
2026-02-18 20:42:45 +0900

    Feat: Executor 설정 추가

--- src/main/java/flipnote/user/global/config/AppConfig.java
+++ src/main/java/flipnote/user/global/config/AppConfig.java
@@ -2,9 +2,12 @@ package flipnote.user.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestClient;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 public class AppConfig {
@@ -17,5 +20,16 @@ public class AppConfig {
     @Bean
     public RestClient restClient() {
         return RestClient.create();
+    }
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(16);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("async-");
+        executor.initialize();
+        return executor;
     }
 }
