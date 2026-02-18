d46f804ed8e1e5026cadea71c32bbcf15de6658d
dungbik <q1w2e30630@gmail.com>
2026-02-19 00:31:44 +0900

    Feat: 인증 헤더 이름 수정

--- src/main/java/flipnote/user/global/constants/HttpConstants.java
+++ src/main/java/flipnote/user/global/constants/HttpConstants.java
@@ -1,11 +1,10 @@
 package flipnote.user.global.constants;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public final class HttpConstants {
-
-    private HttpConstants() {
-    }
-
-    public static final String USER_ID_HEADER = "X-USER-ID";
+    public static final String USER_ID_HEADER = "X-User-Id";
     public static final String ACCESS_TOKEN_COOKIE = "accessToken";
     public static final String REFRESH_TOKEN_COOKIE = "refreshToken";
     public static final String OAUTH_VERIFIER_COOKIE = "oauth2_auth_request";
